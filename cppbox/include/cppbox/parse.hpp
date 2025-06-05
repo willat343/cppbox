2cd8e9532abf31296bdf3388d422d4348f1c3b55
William Talbot <william.talbot97@gmail.com>
2025-06-05 19:29:58 +0200

    added uppercase/lowercase conversion functions

--- cppbox/include/cppbox/parse.hpp
+++ cppbox/include/cppbox/parse.hpp
@@ -16,6 +16,52 @@ namespace cppbox {
  */
 std::vector<std::string> split_by_char(const std::string& input, const char delim);
 
+/**
+ * @brief Convert string to lowercase.
+ *
+ * @param string
+ * @return std::string
+ */
+std::string tolower(const std::string& string);
+
+/**
+ * @brief Convert string (temporary) to lowercase.
+ *
+ * @param string
+ * @return std::string
+ */
+std::string tolower(std::string&& string);
+
+/**
+ * @brief Convert string to lowercase in-place.
+ *
+ * @param string
+ */
+void tolower(std::string& string);
+
+/**
+ * @brief Convert string to uppercase.
+ *
+ * @param string
+ * @return std::string
+ */
+std::string toupper(const std::string& string);
+
+/**
+ * @brief Convert string (temporary) to uppercase.
+ *
+ * @param string
+ * @return std::string
+ */
+std::string toupper(std::string&& string);
+
+/**
+ * @brief Convert string to uppercase in-place.
+ *
+ * @param string
+ */
+void toupper(std::string& string);
+
 }
 
 #endif
--- cppbox/src/parse.cpp
+++ cppbox/src/parse.cpp
@@ -1,5 +1,6 @@
 #include "cppbox/parse.hpp"
 
+#include <algorithm>
 #include <sstream>
 
 namespace cppbox {
@@ -15,6 +16,36 @@ std::vector<std::string> split_by_char(const std::string& input, const char deli
         }
     }
     return output;
+}
+
+std::string tolower(const std::string& input) {
+    std::string output = input;
+    tolower(output);
+    return output;
+}
+
+std::string tolower(std::string&& string) {
+    tolower(string);
+    return std::move(string);
+}
+
+void tolower(std::string& input) {
+    std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return std::tolower(c); });
+}
+
+std::string toupper(const std::string& input) {
+    std::string output = input;
+    toupper(output);
+    return output;
+}
+
+std::string toupper(std::string&& string) {
+    toupper(string);
+    return std::move(string);
+}
+
+void toupper(std::string& input) {
+    std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return std::toupper(c); });
 }
 
 }
