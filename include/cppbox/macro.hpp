73eb46d70d3961cb7d6c9c27078abdd366166e9d
William Talbot <william.talbot97@gmail.com>
2025-10-27 20:56:28 +0100

    added a FOR_EACH macro variant

--- include/cppbox/macro.hpp
+++ include/cppbox/macro.hpp
@@ -4,7 +4,7 @@
 // Add a comma to the end
 #define ADD_COMMA(x) x,
 
-// FE (For each) macros to handle up to 19 arguments
+// FE (FOR_EACH) macros to handle up to 19 arguments
 #define FE0(MACRO)
 #define FE1(MACRO, X) MACRO(X)
 #define FE2(MACRO, X, ...) MACRO(X) FE1(MACRO, __VA_ARGS__)
@@ -39,6 +39,37 @@
     FOR_EACH_IMPL(_0, ##__VA_ARGS__, FE19, FE18, FE17, FE16, FE15, FE14, FE13, FE12, FE11, FE10, FE9, FE8, FE7, FE6, \
             FE5, FE4, FE3, FE2, FE1, FE0)                                                                            \
     (MACRO, ##__VA_ARGS__)
+
+// FEI (FOR_EACH_I) macros to handle up to 19 arguments
+#define FEI0(MACRO, DATA)
+#define FEI1(MACRO, DATA, X) MACRO(DATA, X)
+#define FEI2(MACRO, DATA, X, ...) MACRO(DATA, X) FEI1(MACRO, DATA, __VA_ARGS__)
+#define FEI3(MACRO, DATA, X, ...) MACRO(DATA, X) FEI2(MACRO, DATA, __VA_ARGS__)
+#define FEI4(MACRO, DATA, X, ...) MACRO(DATA, X) FEI3(MACRO, DATA, __VA_ARGS__)
+#define FEI5(MACRO, DATA, X, ...) MACRO(DATA, X) FEI4(MACRO, DATA, __VA_ARGS__)
+#define FEI6(MACRO, DATA, X, ...) MACRO(DATA, X) FEI5(MACRO, DATA, __VA_ARGS__)
+#define FEI7(MACRO, DATA, X, ...) MACRO(DATA, X) FEI6(MACRO, DATA, __VA_ARGS__)
+#define FEI8(MACRO, DATA, X, ...) MACRO(DATA, X) FEI7(MACRO, DATA, __VA_ARGS__)
+#define FEI9(MACRO, DATA, X, ...) MACRO(DATA, X) FEI8(MACRO, DATA, __VA_ARGS__)
+#define FEI10(MACRO, DATA, X, ...) MACRO(DATA, X) FEI9(MACRO, DATA, __VA_ARGS__)
+#define FEI11(MACRO, DATA, X, ...) MACRO(DATA, X) FEI10(MACRO, DATA, __VA_ARGS__)
+#define FEI12(MACRO, DATA, X, ...) MACRO(DATA, X) FEI11(MACRO, DATA, __VA_ARGS__)
+#define FEI13(MACRO, DATA, X, ...) MACRO(DATA, X) FEI12(MACRO, DATA, __VA_ARGS__)
+#define FEI14(MACRO, DATA, X, ...) MACRO(DATA, X) FEI13(MACRO, DATA, __VA_ARGS__)
+#define FEI15(MACRO, DATA, X, ...) MACRO(DATA, X) FEI14(MACRO, DATA, __VA_ARGS__)
+#define FEI16(MACRO, DATA, X, ...) MACRO(DATA, X) FEI15(MACRO, DATA, __VA_ARGS__)
+#define FEI17(MACRO, DATA, X, ...) MACRO(DATA, X) FEI16(MACRO, DATA, __VA_ARGS__)
+#define FEI18(MACRO, DATA, X, ...) MACRO(DATA, X) FEI17(MACRO, DATA, __VA_ARGS__)
+#define FEI19(MACRO, DATA, X, ...) MACRO(DATA, X) FEI18(MACRO, DATA, __VA_ARGS__)
+
+/**
+ * @brief The `FOR_EACH` macro calls `MACRO` on each argument passed in.
+ *
+ */
+#define FOR_EACH_I(MACRO, DATA, ...)                                                                                   \
+    FOR_EACH_IMPL(_0, ##__VA_ARGS__, FEI19, FEI18, FEI17, FEI16, FEI15, FEI14, FEI13, FEI12, FEI11, FEI10, FEI9, FEI8, \
+            FEI7, FEI6, FEI5, FEI4, FEI3, FEI2, FEI1, FEI0)                                                            \
+    (MACRO, DATA, ##__VA_ARGS__)
 
 /**
  * @brief The `GLUE` macro combines two strings together.
