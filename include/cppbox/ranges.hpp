d24c7a2cbc4c8aa9a065f4106d791c954efd5f86
William Talbot <william.talbot97@gmail.com>
2025-12-11 20:15:49 +0100

    fixed typo in concept name

--- include/cppbox/ranges.hpp
+++ include/cppbox/ranges.hpp
@@ -7,7 +7,7 @@
 namespace cppbox {
 
 template<typename Range, typename Element>
-concept FowardRangeOf = std::ranges::forward_range<Range> && std::same_as<std::ranges::range_value_t<Range>, Element>;
+concept ForwardRangeOf = std::ranges::forward_range<Range> && std::same_as<std::ranges::range_value_t<Range>, Element>;
 
 }
 
