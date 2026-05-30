8a6c8c32899858e88531fd235b626e5f22811ba5
William Talbot <william.talbot97@gmail.com>
2026-05-30 21:37:03 +0200

    added array merging function

--- include/cppbox/array.hpp
+++ include/cppbox/array.hpp
@@ -8,6 +8,9 @@ namespace cppbox {
 template<class T, std::size_t N>
 constexpr bool contains(const std::array<T, N>& array);
 
+template<class T, std::size_t... Sizes>
+constexpr std::array<T, (Sizes + ...)> merge(const std::array<T, Sizes>&... array);
+
 }
 
 #include "cppbox/impl/array.hpp"
--- include/cppbox/impl/array.hpp
+++ include/cppbox/impl/array.hpp
@@ -10,6 +10,20 @@ constexpr bool contains(const std::array<T, N>& array, const T& element) {
     return std::find(array.cbegin(), array.cend(), element) != array.cend();
 }
 
+template<class T, std::size_t... Sizes>
+constexpr std::array<T, (Sizes + ...)> merge(const std::array<T, Sizes>&... array) {
+    std::array<T, (Sizes + ...)> merged{};
+    std::size_t index{0};
+    (
+            [&merged, &index](const auto& array) {
+                for (const auto& item : array) {
+                    merged[index++] = item;
+                }
+            }(array),
+            ...);
+    return merged;
+}
+
 }
 
 #endif
